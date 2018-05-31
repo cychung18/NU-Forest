3c3ca41443dd42bbdca401fc873f631c2767ad8e
Chun-Yu Chung <37920900+cychung18@users.noreply.github.com>
2018-05-30 20:37:54 -0500

    Add files via upload

--- js/map.js
+++ js/map.js
@@ -13,3 +13,21 @@ function openModal2() {
 function closeModal2() {
     document.getElementById('myModal2').style.display = "none";
 }
+
+function share_clicked(){
+    // alert("You have clicked share button!");
+    document.getElementById('share_info').style.display = "block";
+}
+
+function share_event(){
+	alert("You have shared your planting tree on Facebook!");
+	document.getElementById('share_info').style.display = "none";
+}
+
+function share_cancel(){
+	document.getElementById('share_info').style.display = "none";
+}
+
+function close_collect_tip(){
+	document.getElementById("collect-tip").style.opacity = "0";
+}
